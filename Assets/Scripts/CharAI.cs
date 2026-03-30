da76432ee5721dcce1fbe8980e97c8e0d805572f
hichan0310 <hichan0310@kaist.ac.kr>
2026-03-30 16:03:41 +0900

    asdf

--- Assets/Scripts/CharAI.cs
+++ Assets/Scripts/CharAI.cs
@@ -4,7 +4,7 @@ namespace Scripts
 {
     public abstract class PlanMaker:ScriptableObject
     {
-        // plan, force가 필요한 상황에서 계속 호출될거임
+        // plan, force가 필요한 상황에서 계속 호출됨
         public abstract bool GetPlan(Actor actor);
         public abstract bool GetForce(Actor actor);
     }
